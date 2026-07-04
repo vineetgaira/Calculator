e2eb1bed9fcef3dae3d0698fa0850f27c229a599
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-04 10:05:09 +0530

    Add substraction

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def user_input():
     valid_choices={1,2,3,4,5}
     while True:
         try:
-            user_choice=int(input("Enter your choice:"))
+            user_choice=float(input("Enter your choice:"))
             if user_choice in valid_choices:
                 return user_choice
             else:
@@ -21,24 +21,34 @@ def user_input():
                 
 def addition():
 
-    numbers=list(map(int,input("Enter the numbers that you want to add separated by spaces :").split()))
+    numbers=list(map(float,input("Enter the numbers that you want to add separated by spaces :").split()))
     sum=0
     for i in numbers:
         sum+=i
     print(f"The sum of your numbers is: {sum}")
 
-def subtraction():
-    pass
+def substraction():
+    number1=float(input("Enter the first number :"))
+    number2=float(input("Enter the number to subsctract from first number:"))
+
+    difference=number1-number2
+
+    print(f"The subtraction of {number1} by {number2} :{difference:.5f}")
 
 def multiplication():
-    numbers_prod=list(map(int,input("Enter the numbers that you want to multiply separated by spaces :").split()))
+    numbers_prod=list(map(float,input("Enter the numbers that you want to multiply separated by spaces :").split()))
     prod=1
     for i in numbers_prod:
        prod *=i 
     print(f"The product of your numbers is: {prod}")
 
 def division():
-    pass
+    number1=float(input("Enter the number you want to divide :"))
+    number2=float(input("Enter the number you want to divide with :"))
+
+    division= number1/number2
+
+    print(f"The division of {number1} by {number2} :{division:.5f}")
 
 def calculator():
     while True:
@@ -46,8 +56,12 @@ def calculator():
         user_choice=user_input()
         if user_choice==1:
             addition()
+        elif user_choice==2:
+            substraction()
         elif user_choice==3:
             multiplication()
+        elif user_choice==4:
+            division()
         elif user_choice==5:
             print("Thanks for using...")
             break
